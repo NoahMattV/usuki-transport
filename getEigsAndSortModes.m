4a48cdcc3411c852801b218f861c18198da2a45d
Noah Van Der Weide <noahmattv@gmail.com>
2019-05-09 23:55:55 -0500

    hmmm

--- getEigsAndSortModes.m
+++ getEigsAndSortModes.m
@@ -22,44 +22,70 @@ fe_j = 1;
 bp_j = 1;
 be_j = 1;
 
-for i = 1:2*Ny
-    if (abs(sort_me_real(i) > 0) && (sort_me_imag(i) > 0)) % x + iy forward propagating
-        s_fp(fp_j,1) = sort_me_real(i) + sort_me_imag(i)*1i;
-        v_fp(fp_j,:) = V(:,i); 
-        fp_j = fp_j+1;
-    elseif (abs(sort_me_real(i) < 1) && (sort_me_imag(i) == 0)) % forward evanescent
-        s_fe(fe_j,1) = sort_me_real(i);
-        v_fe(fe_j,:) = V(:,i); 
-        fe_j = fe_j+1;
-    elseif (abs(sort_me_real(i) > 0) && (sort_me_imag(i) < 0)) % backward propagating
-        s_bp(bp_j,1) = sort_me_real(i) + sort_me_imag(i)*1i;
-        v_bp(bp_j,:) = V(:,i); 
-        bp_j = bp_j+1;
-    elseif (abs(sort_me_real(i) > 1) && (sort_me_imag(i) == 0)) % backward evanescent 
-        s_be(be_j,1) = sort_me_real(i);
-        v_be(be_j,:) = V(:,i); 
-        be_j = be_j+1;
-    else
-        % Do nothing
-    end
 
+for i = 1:2*Ny
+    if ((0.99 < abs(sort_me(i))) && (abs(sort_me(i)) < 1.01)) %propagating
+        if (imag(sort_me(i)) > 0) % forward
+            s_fp(fp_j,1) = sort_me(i);
+            v_fp(:,fp_j) = V(:,i);
+            fp_j = fp_j+1;
+        else % backward
+            s_bp(bp_j,1) = sort_me(i);
+            v_bp(:,bp_j) = V(:,i); 
+            bp_j = bp_j+1;
+        end
+    else % evanescent
+        if (abs(sort_me(i)) < 1) % forward
+            s_fe(fe_j,1) = sort_me(i);
+            v_fe(:,fe_j) = V(:,i); 
+            fe_j = fe_j+1;
+        else % backwards
+            s_be(be_j,1) = sort_me(i);
+            v_be(:,be_j) = V(:,i); 
+            be_j = be_j+1;
+        end
+    end
 end
+
+
+%for i = 1:2*Ny
+%    if ((sort_me_imag(i) > 0)) % x + iy forward propagating
+%        s_fp(fp_j,1) = sort_me_real(i) + sort_me_imag(i)*1i;
+%        v_fp(:,fp_j) = V(:,i); 
+%        fp_j = fp_j+1;
+%    elseif (abs(sort_me_real(i) < 1) && (sort_me_imag(i) == 0)) % forward evanescent
+%        s_fe(fe_j,1) = sort_me_real(i);
+%        v_fe(:,fe_j) = V(:,i); 
+%        fe_j = fe_j+1;
+%    elseif ((sort_me_imag(i) < 0)) % backward propagating
+%        s_bp(bp_j,1) = sort_me_real(i) + sort_me_imag(i)*1i;
+%        v_bp(:,bp_j) = V(:,i); 
+%        bp_j = bp_j+1;
+%    elseif (abs(sort_me_real(i) > 1) && (sort_me_imag(i) == 0)) % backward evanescent 
+%        s_be(be_j,1) = sort_me_real(i);
+%        v_be(:,be_j) = V(:,i); 
+%        be_j = be_j+1;
+%    else
+%        % Do nothing
+%    end
+%
+%end
 
 zeroRows_fp = any(s_fp==0, 2);
 s_fp(zeroRows_fp, :) = [];
-v_fp(zeroRows_fp, :) = [];
+v_fp(:,zeroRows_fp) = [];
 
 zeroRows_fe = any(s_fe==0, 2);
 s_fe(zeroRows_fe, :) = [];
-v_fe(zeroRows_fe, :) = [];
+v_fe(:,zeroRows_fe) = [];
 
 zeroRows_bp = any(s_bp==0, 2);
 s_bp(zeroRows_bp, :) = [];
-v_bp(zeroRows_bp, :) = [];
+v_bp(:,zeroRows_bp) = [];
 
 zeroRows_be = any(s_be==0, 2);
 s_be(zeroRows_be, :) = [];
-v_be(zeroRows_be, :) = [];
+v_be(:,zeroRows_be) = [];
 
 %zeroRows_Vfp = any(v_fp==0, 2);
 %v_fp(zeroRows_Vfp, :) = [];
@@ -74,8 +100,8 @@ v_be(zeroRows_be, :) = [];
 %s_fe = sort(s_fe);
 %s_bp = sort(s_bp);
 %s_be = sort(s_be);
-Vsort = transpose([v_fp;v_fe;v_bp;v_be]);
-%Vsort = ([v_fp;v_fe;v_bp;v_be]);
+%Vsort = transpose([v_fp;v_fe;v_bp;v_be]);
+Vsort = ([v_fp v_fe v_bp v_be]);
 Dsort = [s_fp;s_fe;s_bp;s_be];
 fp_modes = s_fp;
 
--- usuki_transport_top.m
+++ usuki_transport_top.m
@@ -9,7 +9,6 @@
 clc;
 close all;
 clear;
-format long;
 global Nx;
 global Ny;
 
@@ -39,8 +38,8 @@ V = qpcprofile(QW_width,QW_length,QPC_width,QPC_length);
 %hold off;
 
 e = 1.602E-19;
-Ef_steps = 25;
-Ef = linspace(0,50E-3*e,Ef_steps);
+Ef_steps = 10;
+Ef = linspace(0,(50E-3)*e,Ef_steps); % start at 1
 W = 500E-9; % 500 nm wide
 L = 1000E-9; % 1000 nm long
 QPC_L = 50E-9; % QPC contact is 50 nm long
@@ -53,8 +52,6 @@ m0 = 9.11E-31; % [kg]
 m = 0.067*m0; % [kg]
 
 t = (hbar^2)/(2*m*a^2); % Hopping Energy [J]
-
-phi_l = zeros(Ny,1); % column of phi_l,1 to phi_l,Ny
 
 I = eye(Ny);
 Zero = zeros(Ny);
@@ -71,43 +68,41 @@ H_0 = H_0 + H_0p1;
 H_0 = H_0 + H_0n1;
 
 H = zeros(Nx, Ny, Ny);
-%H = zeros(Nx);
-for i = 1:Nx
-   H(i,:,:) = diag(4*t + V(:,i)) + H_0p1 + H_0n1;
-   %H(i,:,:) = H(i,:,:) + H_0p1;
-   %H(i,:,:) = H(i,:,:) + H_0n1;
-end
 
+for i = 1:Nx
+    H(i,:,:) = diag(4*t + V(:,i)) + H_0p1 + H_0n1;
+    %H(i,:,:) = H(i,:,:) + H_0p1;
+    %H(i,:,:) = H(i,:,:) + H_0n1;
+end
 
 H_0_0p1 = diag(t_vector);
 H_0_0n1 = diag(t_vector);
+G = zeros(Ef_steps,1);
 
+for f = 10:Ef_steps
 
 T_11 = zeros(Ny); % 0
 T_12 = eye(Ny); % I
-T_21 = -inv(H_0_0p1)*H_0_0n1; % No B = -I
-%Temp = Ef.*T_12;
-T_22 = (inv(H_0_0p1))*(H_0 - Ef(25).*T_12);
+T_21 = -H_0_0p1\H_0_0n1; % No B = -I
+T_22 = H_0_0p1\(Ef(f)*I - H_0); % for 0th slice
 
 %T = zeros(Ef_steps, Nx, Ny, Ny);
 T22 = zeros(Nx, Ny, Ny);
 T = zeros(Nx, 2*Ny, 2*Ny);
-for i = 1:Nx
-    %for j = 1:Ef_steps
-        temp1 = H(i,:,:);
-        temp2 = reshape(temp1,Ny,Ny);
-        T22(i,:,:) = (inv(H_0_0p1))*(temp2 - Ef(25).*T_12);
-        temp1 = T22(i,:,:);
-        temp2 = reshape(temp1,Ny,Ny);
-        T(i,:,:) = [Zero I ; T_21 temp2]; 
-    %    T(j,i) = [Zero I ; T_21 T22]; 
-    %end
-end
+
+% Defining T slices for 1 to Nx
+%for i = 1:Nx
+%        Htemp = reshape(H(i,:,:),Ny,Ny);
+%        T22(i,:,:) = ((H_0_0p1)^-1)*(Ef(f)*I - Htemp); %Htemp - Ef(f)*I
+%        T22temp = reshape(T22(i,:,:),Ny,Ny);
+%        T(i,:,:) = [Zero I ; T_21 T22temp]; 
+%end
 
 
-T0 = [T_11, T_12 ; T_21, T_22];
+T0 = [T_11 T_12 ; T_21 T_22];
 
-clc;
+% Eigenvalues and Eigenvectors for T0
+%clc;
 fprintf('Getting Eigs and Sorting Modes...');
 [T0_new, Dsort, fp_modes] = getEigsAndSortModes(T0); % mode-to-slice conversion (new T0)
 
@@ -121,8 +116,8 @@ C_2_0 = zeros(Ny);
 
 %P_2_0_1 = T21_new * C_2_0;
 %P_2_0_2 = T22_new;
-P_2_0 = (T21_new * C_2_0 + T22_new)^-1;
-P_1_0 = -1*P_2_0 * T21_new * C_1_0;
+P_2_0 = inv(T21_new * C_2_0 + T22_new);
+P_1_0 = -P_2_0 * T21_new * C_1_0;
 
 P_mat_0 = [I Zero ; P_1_0 P_2_0];
 C_mat_0 = [C_1_0 C_2_0 ; Zero I];
@@ -137,30 +132,23 @@ C_1 = zeros(Nx+2, Ny, Ny);
 C_2 = zeros(Nx+2, Ny, Ny);
 
 for i = 1:Nx  
-  clc;
-  fprintf('%d/%d\n',i,Nx);
-  
   C_mat_temp = reshape(C_mat(i,:,:),2*Ny,2*Ny);
-  
   C_1(i,:,:) = C_mat_temp(1:Ny,1:Ny);
   C_2(i,:,:) = C_mat_temp(1:Ny, Ny+1:2*Ny);
+  C_1temp = C_mat_temp(1:Ny,1:Ny);
+  C_2temp = C_mat_temp(1:Ny, Ny+1:2*Ny);
   
-  C_1temp = reshape(C_1(i,:,:),Ny,Ny);
-  C_2temp = reshape(C_2(i,:,:),Ny,Ny);
-  
-  T22temp = reshape(T22(i,:,:),Ny,Ny);
+  Htemp = reshape(H(i,:,:),Ny,Ny);
+  T22temp = H_0_0p1\(Ef(f)*I - Htemp); %Htemp - Ef(f)*I?
+  Ttemp = [Zero I ; T_21 T22temp]; 
+
   P_2(i,:,:) = inv(T_21*C_2temp + T22temp);
   P2temp = reshape(P_2(i,:,:),Ny,Ny);
-  P_1(i,:,:) = -1*P2temp*T_21*C_1temp;
-  
-  P1temp = reshape(P_1(i,:,:),Ny,Ny);
-  %P2temp = reshape(P_2(i,:,:),Ny,Ny);
-  
+  P_1(i,:,:) = -P2temp*T_21*C_1temp;
+  P1temp = reshape(P_1(i,:,:),Ny,Ny);  
   P_mat = [I Zero ; P1temp P2temp];
-
-  Ttemp = reshape(T(i,:,:),2*Ny,2*Ny);
   
-  C_mat(i+1,:,:) = Ttemp*P_mat*C_mat_temp;
+  C_mat(i+1,:,:) = Ttemp*C_mat_temp*P_mat;
   
 end
 
@@ -171,13 +159,13 @@ C_2(Nx+1,:,:) = C_mat_Nx1(1:Ny, Ny+1:2*Ny);
 C_1_Nx1 = reshape(C_1(Nx+1,:,:),Ny,Ny);
 C_2_Nx1 = reshape(C_2(Nx+1,:,:),Ny,Ny);
 
-T12_Nx1 = T21_new^-1;
+T12_Nx1 = inv(T21_new);
 T22_Nx1 = -T11_new*T12_Nx1;
 T_Nx1 = [Zero T12_Nx1 ; I T22_Nx1];
 T11_Nx1 = T_Nx1(1:Ny, 1:Ny);
 T21_Nx1 = T_Nx1(Ny+1:2*Ny, 1:Ny);
 
-P_2_Nx1 = (T21_Nx1*C_2_Nx1 + T22_Nx1)^-1;
+P_2_Nx1 = inv(T21_Nx1*C_2_Nx1 + T22_Nx1);
 P_1_Nx1 = -P_2_Nx1*T21_Nx1*C_1_Nx1;
 
 P_mat_Nx1 = [I Zero ; P_1_Nx1 P_2_Nx1]; 
@@ -188,8 +176,8 @@ C_mat_Nx2temp = reshape(C_mat(Nx+1,:,:),2*Ny,2*Ny);
 C_1(Nx+2,:,:) = C_mat_Nx2temp(1:Ny,1:Ny);
 C_1_Nx2 = reshape(C_1(Nx+2,:,:),Ny,Ny); % t, matrix of transmission amplitude
 
-Tmn = zeros(size(fp_modes)); % Transmission coefficient matrix (only forward propagating modes)
-mn_length = size(fp_modes,2);
+Tmn = zeros(size(fp_modes,1)); % Transmission coefficient matrix (only forward propagating modes)
+mn_length = size(fp_modes,1);
 k = zeros(mn_length, mn_length);
 v = zeros(mn_length, mn_length);
 G = zeros(mn_length, mn_length);
@@ -213,15 +201,18 @@ for m = 1:mn_length
         
     end
 end
-k_m = log(fp_modes)/(1i*a);
-v_m = hbar*k_m/m;
 
+TmnSum = 0;
 for m = 1:mn_length
     for n = 1:mn_length
-        
-        G(m,n) = (2*e^2/h)*Tmn(m,n);
+        TmnSum = TmnSum + Tmn(m,n);
     end
 end
+
+G(f,1) = (2*e^2/h)*TmnSum;
+clc;
+fprintf('%d/%d\n',f,Ef_steps);
+end %Ef loop
 
 
 % -----------------
